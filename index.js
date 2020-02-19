65629a1405555a61c8403492a0745f6b797d0a5e
Shreyas Shukla <shreyas.shukla26@gmail.com>
2020-02-19 22:13:30 +0530

    Login Automated!

--- index.js
+++ index.js
@@ -6,14 +6,29 @@ const puppeteer = require('puppeteer');
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 768 });
+  // on the instagram landing page
   await page.goto('https://www.instagram.com');
   await page.waitFor('a[href="/accounts/login/?source=auth_switcher"]');
   await page.click('a[href="/accounts/login/?source=auth_switcher"]');
+  await page.waitFor(500);
+
+  // on the login page
+  // for username
+  await page.waitFor('input[name="username"]');
+  await page.type('input[name="username"]', 'logic.b0mb');
+
+  // for password
+  await page.waitFor('input[name="password"]');
+  await page.type('input[name="password"]', 'logicb0mb.pass');
+  // login button
+  await page.click(
+    '#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(4)'
+  );
 
   // await page.type('.gLFyf.gsfi', 'Shreyas', { delay: 100 });
   // await page.keyboard.press('Enter');
   // await page.waitForNavigation();
 
   await page.screenshot({ path: 'example.png' });
-  await browser.close();
+  // await browser.close();
 })();
